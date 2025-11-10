ea67dcc7a7ca340f1f8a310c1f6cb8d913289d1b
KhMansura <iam.khmansura@yandex.com>
2025-11-11 00:17:36 +0600

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,41 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import RootLayout from './layout/RootLayout.jsx';
+import Home from './Components/Home/Home.jsx';
+import AllJobs from './Components/AllJobs/AllJobs.jsx';
+import AuthProvider from './context/AuthProvider.jsx';
+import Register from './Components/Register/Register.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component:RootLayout,
+    children:[
+      {
+        index:true,
+        Component:Home
+      },
+      {
+        path:'allJobs',
+        Component:AllJobs
+      },
+      {
+        path:'register',
+        Component:Register
+      },
+    ]
+  },
+]);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
+  </StrictMode>,
+)
